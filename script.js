fcdd28f1b0d65386707054bfc934109262719a58
Dintyo <46850979+dintyo@users.noreply.github.com>
2022-11-05 08:38:18 +1000

    added image for footer

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+///////////////////////////////////////
+// define DOM elements that we'll use
+
+const scoreText = document.querySelector(".score");
+console.log(scoreText.textContent);
