c0c5109d912a774098a23007a42ffe2725f74927
Dintyo <46850979+dintyo@users.noreply.github.com>
2022-11-05 08:46:37 +1000

    starting main functions

--- script.js
+++ script.js
@@ -4,4 +4,37 @@
 // define DOM elements that we'll use
 
 const scoreText = document.querySelector(".score");
-console.log(scoreText.textContent);
+const userRock = document.querySelector(".rock");
+const userPaper = document.querySelector(".paper");
+const userScissors = document.querySelector(".scissors");
+const playerMove = document.querySelector(".player-move");
+const computerMove = document.querySelector(".computer-move");
+
+///////////////////////////////////////
+// My Plan
+/*
+- Start with function getComputerChoice - randomly return Rock, Paper or Scissors. 
+
+- Add event listeners to buttons for users input
+
+- Set users score based on button input
+
+- Write a function to determine the winner
+
+- Log the score
+
+- Print the inputs to the screen.
+
+*/
+
+///////////////////////////////////////
+// Main code
+
+// define global variables used for playerMove and computerMove
+
+let pMove;
+let cMove;
+
+// function to get computers input
+
+const getComputerMove = function () {};
