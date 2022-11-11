a91ccca086e6f486e63ed50347842af95f47f760
Dintyo <46850979+dintyo@users.noreply.github.com>
2022-11-11 18:57:45 +1000

    added sounds and some animation

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ const playerMove = document.querySelector(".player-move");
 const computerMove = document.querySelector(".computer-move");
 const btnPlayAgain = document.querySelector(".btn-play");
 
+////////////////////////////////////////
+// Sounds
+
+const audioDraw = new Audio("sounds/draw.wav");
+const audioLoseRound = new Audio("sounds/lose-round.wav");
+const audioWinRound = new Audio("sounds/win-round.wav");
+const audioWinGame = new Audio("sounds/win-game.wav");
+const audioLoseGame = new Audio("sounds/lose-game.wav");
+
 ///////////////////////////////////////
 // Main code
 
@@ -38,39 +47,82 @@ const getComputerMove = function () {
 
 const printResult = function (player, move) {
   if (pScore < 5 && cScore < 5) {
+    let counter = 0;
+    userRock.disabled = true;
+    userScissors.disabled = true;
+    userPaper.disabled = true;
+    const randomMoves = function () {
+      if (counter == 9) {
+        clearInterval(randomComputerMove);
+        if (player === computerMove) {
+          if (move === "Scissors") player.textContent = "✂️";
+          if (move === "Rock") player.textContent = "🪨";
+          if (move === "Paper") player.textContent = "🖐️";
+          updateScore(getWinner());
+        }
+      } else {
+        if (counter % 3 === 0) computerMove.textContent = "✂️";
+        if (counter % 3 === 1) computerMove.textContent = "🪨";
+        if (counter % 3 === 2) computerMove.textContent = "🖐️";
+        counter++;
+        console.log(counter);
+      }
+    };
+
+    const randomComputerMove = setInterval(randomMoves, 100);
+
     if (move === "Scissors") player.textContent = "✂️";
     if (move === "Rock") player.textContent = "🪨";
     if (move === "Paper") player.textContent = "🖐️";
-
-    let roundResult = getWinner();
-  } else {
-    // show button to play again and reset game
   }
+
+  let roundResult = getWinner();
 };
 
 // function to update the score
 
+scoreText.addEventListener("transitionend", () => {
+  scoreText.classList.remove("scoreupdate");
+});
+
 const updateScore = function (result) {
   if (pScore < 5 && cScore < 5) {
+    userRock.disabled = false;
+    userScissors.disabled = false;
+    userPaper.disabled = false;
+    scoreText.classList.add("scoreupdate");
     if (result === "Win") {
       pScore++;
       console.log(pScore);
+      audioWinRound.currentTime = 0;
+      audioWinRound.play();
       scoreText.innerHTML = `you won 😃 <br> the score is now ${pScore} - ${cScore}`;
       if (pScore === 5) {
+        audioWinGame.currentTime = 0;
+        audioWinGame.play();
         scoreText.innerHTML = `you won 😃 <br> the final score is ${pScore} - ${cScore}`;
         btnPlayAgain.classList.remove("hidden");
+        userRock.disabled = true;
+        userScissors.disabled = true;
+        userPaper.disabled = true;
       }
       return;
     }
     if (result === "Loss") {
       cScore++;
+      audioLoseRound.currentTime = 0;
+      audioLoseRound.play();
       scoreText.innerHTML = `you lost 😞 <br> the score is now ${pScore} - ${cScore}`;
       if (cScore === 5) {
+        audioLoseGame.currentTime = 0;
+        audioLoseGame.play();
         scoreText.innerHTML = `you lost 😞 <br> the final score is ${pScore} - ${cScore}`;
         btnPlayAgain.classList.remove("hidden");
       }
       return;
     }
+    audioDraw.currentTime = 0;
+    audioDraw.play();
     scoreText.innerHTML = `you tied 😕 <br> the score is now ${pScore} - ${cScore}`;
   } else {
   }
@@ -110,7 +162,7 @@ userRock.addEventListener("click", function () {
   getComputerMove();
   printResult(playerMove, pMove);
   printResult(computerMove, cMove);
-  updateScore(getWinner());
+  // updateScore(getWinner());
 });
 
 userPaper.addEventListener("click", function () {
@@ -118,7 +170,7 @@ userPaper.addEventListener("click", function () {
   getComputerMove();
   printResult(playerMove, pMove);
   printResult(computerMove, cMove);
-  updateScore(getWinner());
+  // updateScore(getWinner());
 });
 
 userScissors.addEventListener("click", function () {
@@ -126,7 +178,7 @@ userScissors.addEventListener("click", function () {
   getComputerMove();
   printResult(playerMove, pMove);
   printResult(computerMove, cMove);
-  updateScore(getWinner());
+  // updateScore(getWinner());
 });
 
 btnPlayAgain.addEventListener("click", function () {
@@ -136,4 +188,7 @@ btnPlayAgain.addEventListener("click", function () {
   playerMove.textContent = "😎";
   computerMove.textContent = "💻";
   btnPlayAgain.classList.add("hidden");
+  userRock.disabled = false;
+  userScissors.disabled = false;
+  userPaper.disabled = false;
 });
